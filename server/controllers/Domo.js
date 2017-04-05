2d7d59b8b66a1afaacb077315de3c0645726d90a
Zachary Wilken <zxw8994@rit.edu>
2017-04-05 13:38:49 -0400

    Adding RAWR!

---
+++ server/controllers/Domo.js
@@ -0,0 +1,48 @@
+
+const models = require('../models');
+
+const Domo = models.Domo;
+
+const makerPage = (req, res) => {
+  Domo.DomoModel.findByOwner(req.session.account._id, (err, docs) => {
+    if (err) {
+      console.log(err);
+      return res.status(400).json({ error: 'An error occurred' });
+    }
+
+    return res.render('app', { domos: docs });
+  });
+};
+
+const makeDomo = (req, res) => {
+  if (!req.body.name || !req.body.age) {
+    return res.status(400).json({ error: 'RAWR! Both name and age are required' });
+  }
+
+  const domoData = {
+    name: req.body.name,
+    age: req.body.age,
+    owner: req.session.account._id,
+  };
+
+  const newDomo = new Domo.DomoModel(domoData);
+
+  const domoPromise = newDomo.save();
+
+  domoPromise.then(() => res.json({ redirect: '/maker' }));
+
+  domoPromise.catch((err) => {
+    console.log(err);
+    if (err.code === 11000) {
+      return res.status(400).json({ error: 'Domo already exists.' });
+    }
+
+    return res.status(400).json({ error: 'An error occurred' });
+  });
+
+  return domoPromise;
+};
+
+
+module.exports.makerPage = makerPage;
+module.exports.make = makeDomo;
